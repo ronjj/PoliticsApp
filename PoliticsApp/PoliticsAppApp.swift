8467b230c25300045e9f352f2450b38f0ba13773
Ronald Jabouin <ronaldjabouin@dhcp-vl2041-1300.eduroam.cornell.edu>
2022-11-18 08:29:50 -0500

    second commit

---
+++ PoliticsApp/PoliticsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoliticsAppApp.swift
+//  PoliticsApp
+//
+//  Created by Ronald Jabouin on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PoliticsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
