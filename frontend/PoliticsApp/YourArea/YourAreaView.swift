35af254003a8cf94b56e4d0a51fac2a4e632f219
mluo24 <miranda.luo24@gmail.com>
2022-11-20 19:18:59 -0500

    initialized YourAreaView

---
+++ frontend/PoliticsApp/YourArea/YourAreaView.swift
@@ -0,0 +1,20 @@
+//
+//  YourAreaView.swift
+//  PoliticsApp
+//
+//  Created by Miranda Luo on 11/20/22.
+//
+
+import SwiftUI
+
+struct YourAreaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct YourAreaView_Previews: PreviewProvider {
+    static var previews: some View {
+        YourAreaView()
+    }
+}
