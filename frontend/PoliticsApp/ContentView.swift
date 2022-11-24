98bbbfe5a76b99ea6a5a056581d1813b8298a07c
Ronald Jabouin <ronaldjabouin2004@gmail.com>
2022-11-23 21:29:52 -0500

    remade contentview. got lost in last commit

---
+++ frontend/PoliticsApp/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  PoliticsApp
+//
+//  Created by Ronald Jabouin on 11/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView{
+            replace1()
+                .tabItem{
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            
+            NewsView()
+                .tabItem{
+                    Image(systemName: "newspaper")
+                    Text("News")
+                }
+            
+            replace2()
+                .tabItem{
+                    Image(systemName: "mappin")
+                    Text("Your Area")
+                }
+        }
+    }
+}
+
