de6c53f24aa3f13f47d26639b922fa7ad3842177
mluo24 <miranda.luo24@gmail.com>
2022-12-02 17:34:26 -0500

    integrated news

--- frontend/PoliticsApp/YourArea/YourAreaView.swift
+++ frontend/PoliticsApp/YourArea/YourAreaView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct YourAreaView: View {
     
     // insert getting articles here
-
+    @StateObject var articlesNewsVM = ArticleNewsViewModel()
+    
     var body: some View {
         NavigationView{
-            ScrollView{
+//            ScrollView{
                 VStack {
                     PoliticianRowView(polType: "House", politicians: ["Pol 1"])
                     PoliticianRowView(polType: "Senate", politicians: ["Pol 1", "Pol 2"])
@@ -23,8 +24,12 @@ struct YourAreaView: View {
                             .padding()
                         Spacer()
                     }
-                   // ArticleListView(articles: vm.articles )
-                }
+                    ArticleListView(articles: articles)
+                        .overlay(overlayView)
+                    //whenever the selected category chnages, the data is reloaded
+                        .task(id: articlesNewsVM.fetchTaskToken, loadTask)
+                        .refreshable(action: refreshTask)
+//                }
             }
             .navigationTitle("Your Area")
             .navigationBarTitleDisplayMode(.large)
@@ -36,6 +41,38 @@ struct YourAreaView: View {
                 }
             }
         }
+    }
+    
+    private var articles: [Article] {
+        if case let .success(articles) = articlesNewsVM.phase {
+            return articles
+        } else {
+            return []
+        }
+    }
+    
+    @ViewBuilder
+    private var overlayView: some View {
         
+        switch articlesNewsVM.phase {
+        case .empty:
+            ProgressView()
+            
+        case .success(let articles) where articles.isEmpty:
+            EmptyPlaceholderView(text: "No Articles", image: nil)
+        case .failure(let error):
+            RetryView(text: error.localizedDescription, retryAction: refreshTask)
+            
+            
+        default:  EmptyView()
+        }
+    }
+        
+    private func loadTask() async {
+        await articlesNewsVM.loadArticles()
+    }
+    
+    private func refreshTask() {
+        articlesNewsVM.fetchTaskToken = FetchTaskToken(category: articlesNewsVM.fetchTaskToken.category, token: Date())
     }
 }
