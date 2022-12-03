05c5c0259c410ac08c30d9ef19ab5c9e1027f3c7
mluo24 <miranda.luo24@gmail.com>
2022-12-02 19:51:50 -0500

    Revert "fixed Your Area formatting"
    This reverts commit 7151cb84de6b85ec18cd5918d6a5927223c47408.

--- frontend/PoliticsApp/YourArea/SettingsView.swift
+++ frontend/PoliticsApp/YourArea/SettingsView.swift
@@ -8,12 +8,10 @@
 import SwiftUI
 
 struct SettingsView: View {
-    
     @ObservedObject var userVM = UserVM()
     var body: some View {
         VStack{
             Text("Your zip code is \(userVM.user.zipCode).")
         }
     }
-    
 }
